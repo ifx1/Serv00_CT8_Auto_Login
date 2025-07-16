b88d3d1e8af9c673cc4320dac6905aeff1036041
ifx1 <neoiz@outlook.com>
2025-07-16 16:49:11 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -106,7 +106,7 @@ async def main():
         status_text = "登录成功" if is_logged_in else "登录失败"
         
         message += (
-            f"{status_icon} 账号: `{username}`  ({serviceName}) \n"
+            f"{status_icon} 账号: `{username}`  ({serviceName})\n"
         )
 
         delay = random.randint(1000, 8000)
@@ -122,7 +122,7 @@ async def send_telegram_message(message, account_count, success_count, failed_co
     formatted_message = f"""
 📨 *Serv00 & CT8 保号脚本运行报告*
 ⏰ *北京时间*: `{format_to_iso(datetime.utcnow() + timedelta(hours=8))}`
-📊 *共计*: {account_count} | ✅ *成功*: {success_count} | ❌ *失败*: {failed_count}
+📊 *共计: {account_count} | ✅ 成功: {success_count} | ❌ 失败: {failed_count}*
 ━━━━━━━━━━━━━━━━━━
 {message}
 """
